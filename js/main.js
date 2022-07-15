f4c561a3423c5b08d0d6a946b109e58e83a60626
filonov404 <fffilonov31@gmail.com>
2022-07-15 11:33:40 +0300

    222

--- js/main.js
+++ js/main.js
@@ -1,7 +1,34 @@
+//тогл бургер
+
 $(".menu-mobile").on("click", function () {
     $(".header-bottom").css("display", "block");
 });
 
 $(".close-btn").on("click", function () {
     $(".header-bottom").css("display", "none");
+});
+
+//Слайдер на глалвной
+new Swiper(".slider-wrapper", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+        currentClass: "all-items",
+        totalClass: "active-item",
+    },
+    navigation: {
+        nextEl: ".arrow-next",
+        prevEl: ".arrow-prew",
+        disabledClass: "arrow-disabled"
+    },
+    // breakpoints: {
+    //     slidesPerView: 1,
+    //     spaceBetween: 5,
+    //     550: {
+    //         slidesPerView: 1.05,
+    //         spaceBetween: 5,
+    //     }
+    // }
 });
